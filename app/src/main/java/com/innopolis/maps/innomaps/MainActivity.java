fca6c96039facf0e64228515cebf22d989e2fbe5
neewy <n.yushkevich@hotmail.com>
2016-01-24 22:53:30 +0300

    Revert repository

--- app/src/main/java/com/innopolis/maps/innomaps/MainActivity.java
+++ app/src/main/java/com/innopolis/maps/innomaps/MainActivity.java
@@ -17,4 +17,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
+
+    public void openWiFi(View view) {
+        Intent intent = new Intent(this, WiFiActivity.class);
+        startActivity(intent);
+    }
 }
