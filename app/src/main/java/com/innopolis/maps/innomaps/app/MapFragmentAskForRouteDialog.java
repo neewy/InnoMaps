18e30513fff783f6ece1f5a6ac013331102a20f2
Ziyoiddin Yusupov <ziyoiddin.yusupov@gmail.com>
2016-02-25 14:55:05 +0300

    - Disable the Google maps button on map (on pin clicked should work the same as Go button)   - Page with event description: Add GO button in the lower-right corner which opens a map showing the route to event (for now use any sample location as a starting point and location of the event as an ending point)   - Add a telegram link to open group links and user profile links (remove the group link itself)

---
+++ app/src/main/java/com/innopolis/maps/innomaps/app/MapFragmentAskForRouteDialog.java
@@ -0,0 +1,30 @@
+package com.innopolis.maps.innomaps.app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Ziyoiddin on 26-Feb-16.
+ */
+public class MapFragmentAskForRouteDialog extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        return new AlertDialog.Builder(getActivity())
+                .setMessage("Take to " + getArguments().getString("dialogText"))
+                .setPositiveButton("Route", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //TODO need to make a route in main screen
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MapFragmentAskForRouteDialog.this.getDialog().cancel();
+                    }
+                })
+                .create();
+    }
+
+}
