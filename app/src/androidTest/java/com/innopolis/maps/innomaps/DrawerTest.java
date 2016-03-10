e81753de4f150c126ff949d52fbfed2f87ad69f7
Maxim Shalavin <shalavinms@gmail.com>
2016-03-10 17:09:17 +0300

    Testing all actions on item of the drawer

--- app/src/androidTest/java/com/innopolis/maps/innomaps/DrawerTest.java
+++ app/src/androidTest/java/com/innopolis/maps/innomaps/DrawerTest.java
@@ -3,6 +3,7 @@ package com.innopolis.maps.innomaps;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.core.deps.guava.base.Preconditions;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.innopolis.maps.innomaps.app.MainActivity;
@@ -10,8 +11,10 @@ import com.innopolis.maps.innomaps.app.MainActivity;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerActions.closeDrawer;
 import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
@@ -33,18 +36,9 @@ public class DrawerTest extends ActivityInstrumentationTestCase2<MainActivity> {
         getActivity();
     }
 
-    public void testEnterName() throws Exception {
-
-        onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
-
-        openDrawer(R.id.drawer_layout);
-        onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
-        closeDrawer(R.id.drawer_layout);
-
-    }
 
     @SuppressWarnings("unchecked")
-    public void testOpenDrawer_clickItem() {
+    public void testEvent() {
         openDrawer(R.id.drawer_layout);
 
         String rowContents = "Events";
@@ -53,10 +47,10 @@ public class DrawerTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
             ListView listEvent = (ListView) getActivity().findViewById(R.id.eventList);
             Preconditions.checkNotNull(listEvent, "EventList is null");
-            for (int i = 0; i <listEvent.getCount()-1; i++) {
+            for (int i = 0; i < listEvent.getCount() - 1; i++) {
                 onData(anything()).inAdapterView(withId(R.id.eventList)).atPosition(i).perform(click());
                 Thread.sleep(100);
-                //CHeak empty field
+                //Cheak empty field
                 onView(withId(R.id.eventName)).check(matches(isDisplayed()));
                 onView(withId(R.id.description)).check(matches(isDisplayed()));
                 onView(withId(R.id.timeLeft)).check(matches(isDisplayed()));
@@ -71,5 +65,43 @@ public class DrawerTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     }
 
+    public void testOpenDrawer() {
+        String[] rowContents = {"Events", "Maps", "Favourite"};
+        String[] namedAction = {"longClick()", "scrollTo()", "doubleClick()"};
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
+        openDrawer(R.id.drawer_layout);
+        onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
+        for (int i = 0; i < rowContents.length; i++) {
+
+            onView(withText(rowContents[i])).perform(click());
+
+            for (int j = 0; j < 3; j++) {
+                openDrawer(R.id.drawer_layout);
+                try {
+                    switch (j) {
+                        case 0: {
+                            onView(withText(rowContents[i])).perform(longClick());
+                            break;
+                        }
+                        case 1: {
+                            onView(withText(rowContents[i])).perform(scrollTo());
+                            break;
+                        }
+                        case 2: {
+                            onView(withText(rowContents[i])).perform(doubleClick());
+                            break;
+                        }
+                    }
+
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    Log.d("MyTestLog", "action " + namedAction[j] + " is not exist for " + rowContents[i]);
+                    openDrawer(R.id.drawer_layout);
+                }
+            }
+
+        }
+    }
 
 }
+
