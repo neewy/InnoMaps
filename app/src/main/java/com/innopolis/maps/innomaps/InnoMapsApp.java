342bf302f10448280a5c277c77d7b9d1d65a3ddf
neewy <n.yushkevich@hotmail.com>
2016-04-16 21:12:14 +0300

    Gradle hotfix

--- app/src/main/java/com/innopolis/maps/innomaps/InnoMapsApp.java
+++ app/src/main/java/com/innopolis/maps/innomaps/InnoMapsApp.java
@@ -6,17 +6,8 @@ import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
-/**
- * Created by Nikolay on 12.03.2016.
- */
-
 @ReportsCrashes(
-        formUri = "http://collector.tracepot.com/95d77fd5",
-        reportType = org.acra.sender.HttpSender.Type.FORM,
-        httpMethod = org.acra.sender.HttpSender.Method.POST,
-        formUriBasicAuthLogin="reporter",
-        formUriBasicAuthPassword="InnoMaps",
-
+        formUri = "https://collector.tracepot.com/95d77fd5",
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.crash_toast_text
 )
