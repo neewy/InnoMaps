39d34fc39475f04c80f2fd2bef3e5abc555370da
Maxim Shalavin <shalavinms@gmail.com>
2016-04-27 09:29:30 +0300

    hide keyboard

--- app/src/main/java/com/innopolis/maps/innomaps/maps/MapsFragment.java
+++ app/src/main/java/com/innopolis/maps/innomaps/maps/MapsFragment.java
@@ -393,6 +393,7 @@ public class MapsFragment extends MarkersAdapter implements ActivityCompat.OnReq
             }
         });
     }
+
     private static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
