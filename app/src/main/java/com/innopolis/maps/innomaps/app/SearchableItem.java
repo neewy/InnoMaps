44379b3b68af8fb8dea350b44bd4c74b662a7ac4
akozlovainno <a.kozlova@innopolis.ru>
2016-06-07 15:59:55 +0400

    Update SearchableItem.java

--- app/src/main/java/com/innopolis/maps/innomaps/app/SearchableItem.java
+++ app/src/main/java/com/innopolis/maps/innomaps/app/SearchableItem.java
@@ -103,6 +103,7 @@ public class SearchableItem implements Comparable<SearchableItem> {
         }
     }
 
+    // TODO: Extract string constants
     public static void addPois(List<SearchableItem> items, List<HashMap<String, String>> pois) {
         for (HashMap<String, String> poi : pois) {
             SearchableItem searchableItem = new SearchableItem();
