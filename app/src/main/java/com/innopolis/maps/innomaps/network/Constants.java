a1ad8d666751af702f7c4f5e30b87634741d379a
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-08 20:09:40 +0300

    Added jackson-databind lib in build.gradle for JSON deserialization

---
+++ app/src/main/java/com/innopolis/maps/innomaps/network/Constants.java
@@ -0,0 +1,7 @@
+package com.innopolis.maps.innomaps.network;
+
+/**
+ * Created by alnedorezov on 6/8/16.
+ */
+public class Constants {
+}
--- app/src/main/java/com/innopolis/maps/innomaps/pathfinding/LatLngGraphVertex.java
+++ app/src/main/java/com/innopolis/maps/innomaps/pathfinding/LatLngGraphVertex.java
@@ -1,47 +1,7 @@
 package com.innopolis.maps.innomaps.pathfinding;
 
-import com.google.android.gms.maps.model.LatLng;
-
 /**
- * Implements custom graph vertex for JGraphT based on DefaultEdge class.
- * Necessary for export and import functions.
- * Created by luckychess on 2/28/16.
+ * Created by alnedorezov on 6/8/16.
  */
 public class LatLngGraphVertex {
-    private JGraphTWrapper.GraphElementType graphVertexType;
-    private LatLng vertex;
-    private int vertexId;
-
-    public LatLngGraphVertex(LatLng vertex, int vertexId, JGraphTWrapper.GraphElementType graphVertexType) {
-        this.vertex = vertex;
-        this.vertexId = vertexId;
-        this.graphVertexType = graphVertexType;
-    }
-
-    public LatLngGraphVertex(LatLngGraphVertex vertex) {
-        this.vertex = vertex.getVertex();
-        this.vertexId = vertex.getVertexId();
-    }
-
-    public LatLng getVertex() {
-        return vertex;
-    }
-
-    public int getVertexId() {
-        return vertexId;
-    }
-
-    public JGraphTWrapper.GraphElementType getGraphVertexType() {
-        return graphVertexType;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return getClass() == o.getClass() && ((LatLngGraphVertex)o).getVertex().equals(getVertex());
-    }
-
-    @Override
-    public int hashCode() {
-        return vertex.hashCode();
-    }
 }
