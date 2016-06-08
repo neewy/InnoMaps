ddbee40f38f7d32d9a885c1dfd3bb57c789d0333
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-08 20:11:17 +0300

    Added GraphElementType enum to LatLngGraphVertex (from removed JGraphTWrapper)

--- app/src/main/java/com/innopolis/maps/innomaps/pathfinding/LatLngGraphVertex.java
+++ app/src/main/java/com/innopolis/maps/innomaps/pathfinding/LatLngGraphVertex.java
@@ -1,7 +1,51 @@
 package com.innopolis.maps.innomaps.pathfinding;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
- * Created by alnedorezov on 6/8/16.
+ * Implements custom graph vertex for JGraphT based on DefaultEdge class.
+ * Necessary for export and import functions.
+ * Created by luckychess on 2/28/16.
  */
 public class LatLngGraphVertex {
+    public enum GraphElementType {
+        DEFAULT, ELEVATOR, STAIRS
+    }
+
+    private GraphElementType graphVertexType;
+    private LatLng vertex;
+    private int vertexId;
+
+    public LatLngGraphVertex(LatLng vertex, int vertexId, GraphElementType graphVertexType) {
+        this.vertex = vertex;
+        this.vertexId = vertexId;
+        this.graphVertexType = graphVertexType;
+    }
+
+    public LatLngGraphVertex(LatLngGraphVertex vertex) {
+        this.vertex = vertex.getVertex();
+        this.vertexId = vertex.getVertexId();
+    }
+
+    public LatLng getVertex() {
+        return vertex;
+    }
+
+    public int getVertexId() {
+        return vertexId;
+    }
+
+    public GraphElementType getGraphVertexType() {
+        return graphVertexType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return getClass() == o.getClass() && ((LatLngGraphVertex) o).getVertex().equals(getVertex());
+    }
+
+    @Override
+    public int hashCode() {
+        return vertex.hashCode();
+    }
 }
