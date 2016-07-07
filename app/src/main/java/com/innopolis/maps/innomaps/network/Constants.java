958699114238461748a27d15e4806c3c78e9d2b8
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-07-07 22:18:27 +0300

    Resolved issues reported by SonarQube about network/Constants.java

--- app/src/main/java/com/innopolis/maps/innomaps/network/Constants.java
+++ app/src/main/java/com/innopolis/maps/innomaps/network/Constants.java
@@ -4,57 +4,64 @@ package com.innopolis.maps.innomaps.network;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-public interface Constants {
-    String LOG = "Exception";
-    String CONNECTION_PROTOCOL = "https";
-    String IP = "188.130.155.224";
-    String PORT = "9000";
-    char PARAMETER_DELIMITER = '&';
-    char PARAMETER_EQUALS_CHAR = '=';
-    String ENCODING = "UTF-8";
-    String COLON_AND_TWO_SLASHES = "://";
-    char COLON = ':';
-    char QUESTION_MARK = '?';
-    String SLASH_RESOURCES_SLASH = "/resources/";
-    String LATITUDE = "latitude";
-    String LONGITUDE = "longitude";
-    String FLOOR = "floor";
-    String CLOSEST_POINT_FROM_GRAPH = "closestPointFromGraph";
-    String FLOOR_CALCULATION_ERROR = "The error arises on calculation floor for the following coordinates:";
+public final class Constants {
+    public static final String LOG = "Exception";
+    public static final String CONNECTION_PROTOCOL = "https";
+    public static final String IP = "188.130.155.224";
+    public static final String PORT = "9000";
+    public static final char PARAMETER_DELIMITER = '&';
+    public static final char PARAMETER_EQUALS_CHAR = '=';
+    public static final String ENCODING = "UTF-8";
+    public static final String COLON_AND_TWO_SLASHES = "://";
+    public static final char COLON = ':';
+    public static final char QUESTION_MARK = '?';
+    public static final String SLASH_RESOURCES_SLASH = "/resources/";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String FLOOR = "floor";
+    public static final String CLOSEST_POINT_FROM_GRAPH = "closestPointFromGraph";
+    public static final String FLOOR_CALCULATION_ERROR = "The error arises on calculation floor for the following coordinates:";
 
-    String USER_AGENT = "User-Agent";
-    String CONNECTION = "Connection";
-    String POST = "POST";
-    String CONTENT_TYPE = "Content-Type";
-    String CONTENT_TYPE_VALUE = "application/x-www-form-urlencoded";
+    public static final String USER_AGENT = "User-Agent";
+    public static final String CONNECTION = "Connection";
+    public static final String POST = "POST";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_TYPE_VALUE = "application/x-www-form-urlencoded";
 
-    String QUESTION_MARK_ID_EQUALS = "?id=";
-    String COORDINATE = "coordinate";
-    String TYPE = "type";
-    String EDGE = "edge";
-    String ROOM = "room";
-    String STREET = "street";
-    String BUILDING = "building";
-    String PHOTO = "photo";
-    String CREATOR = "creator";
-    String EVENT = "event";
-    String SCHEDULE = "schedule";
-    String FLOOR_OVERLAY = "flooroverlay";
-    String APPOINTMENT = "appointment";
-    char S_LOWERCASE = 's';
-    String CREATED_AFTER_DATE = "createdAfterDate";
-    String SHORTEST_PATH_URL = "%1$s://%2$s:%3$s/resources/shortestPath";
+    public static final String QUESTION_MARK_ID_EQUALS = "?id=";
+    public static final String COORDINATE = "coordinate";
+    public static final String TYPE = "type";
+    public static final String EDGE = "edge";
+    public static final String ROOM = "room";
+    public static final String STREET = "street";
+    public static final String BUILDING = "building";
+    public static final String PHOTO = "photo";
+    public static final String CREATOR = "creator";
+    public static final String EVENT = "event";
+    public static final String SCHEDULE = "schedule";
+    public static final String FLOOR_OVERLAY = "flooroverlay";
+    public static final String APPOINTMENT = "appointment";
+    public static final char S_LOWERCASE = 's';
+    public static final String CREATED_AFTER_DATE = "createdAfterDate";
+    public static final String SHORTEST_PATH_URL = "%1$s://%2$s:%3$s/resources/shortestPath";
 
-    char SPACE = ' ';
-    String ULR_ENCODED_SPACE = "%20";
-    String URL_ENCODED_COLON = "%3A";
+    public static final char SPACE = ' ';
+    public static final String ULR_ENCODED_SPACE = "%20";
+    public static final String URL_ENCODED_COLON = "%3A";
 
-    String VERTEX_ONE_LAT = "vertexOneLatitude";
-    String VERTEX_ONE_LNG = "vertexOneLongitude";
-    String VERTEX_ONE_FLR = "vertexOneFloor";
-    String VERTEX_TWO_LAT = "vertexTwoLatitude";
-    String VERTEX_TWO_LNG = "vertexTwoLongitude";
-    String VERTEX_TWO_FLR = "vertexTwoFloor";
+    public static final String VERTEX_ONE_LAT = "vertexOneLatitude";
+    public static final String VERTEX_ONE_LNG = "vertexOneLongitude";
+    public static final String VERTEX_ONE_FLR = "vertexOneFloor";
+    public static final String VERTEX_TWO_LAT = "vertexTwoLatitude";
+    public static final String VERTEX_TWO_LNG = "vertexTwoLongitude";
+    public static final String VERTEX_TWO_FLR = "vertexTwoFloor";
 
-    SimpleDateFormat serverDateFormat = new SimpleDateFormat("yyyy-MM-dd H:mm:ss.S", Locale.ENGLISH);
+    public static final SimpleDateFormat serverDateFormat = new SimpleDateFormat("yyyy-MM-dd H:mm:ss.S", Locale.ENGLISH);
+
+    // Utility classes, which are a collection of static members, are not meant to be instantiated
+    private Constants() {
+        // Even abstract utility classes, which can be extended, should not have public constructors.
+        // Java adds an implicit public constructor to every class which does not define at least one explicitly.
+        // Hence, at least one non-public constructor should be defined
+    }
 }
