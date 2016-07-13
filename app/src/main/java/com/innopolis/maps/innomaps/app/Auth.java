494b57c355ed4b8a5112c014e64878b509d562b7
soberdeer <antongraywing@gmail.com>
2016-07-14 00:31:14 +0300

    auth layout

--- app/src/main/java/com/innopolis/maps/innomaps/app/Auth.java
+++ app/src/main/java/com/innopolis/maps/innomaps/app/Auth.java
@@ -1,7 +1,6 @@
 package com.innopolis.maps.innomaps.app;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -12,27 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.support.v4.app.FragmentTransaction;
 
 import com.innopolis.maps.innomaps.R;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class Login extends Fragment {
+public class Auth extends Fragment {
 
-    private static final String TAG = "Login";
+    private static final String TAG = "Authentication";
 
-    @InjectView(R.id.input_email)
+    @InjectView(R.id.input_auth_code)
     EditText editText;
-    @InjectView(R.id.input_password)
-    EditText passwordText;
-    @InjectView(R.id.btn_login)
-    Button loginButton;
-    @InjectView(R.id.link_signup)
-    TextView signupLink;
+    @InjectView(R.id.btn_confirm)
+    Button confirmButton;
 
 
     @Override
@@ -49,11 +42,11 @@ public class Login extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.login, container, false);
+        View view = inflater.inflate(R.layout.auth, container, false);
 
         ButterKnife.inject(this, view);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        confirmButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -61,39 +54,27 @@ public class Login extends Fragment {
             }
         });
 
-        signupLink.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Fragment fragment = new SignUp();
-                FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
-                fragTransaction.replace(R.id.content_frame, fragment);
-                fragTransaction.addToBackStack(null);
-                fragTransaction.commit();
-            }
-        });
         return view;
     }
 
 
     public void login() {
-        Log.d(TAG, getContext().getString(R.string.login));
+        Log.d(TAG, getContext().getString(R.string.auth));
 
         if (!validate()) {
-            onLoginFailed();
+            onConfirmFailed();
             return;
         }
 
-        loginButton.setEnabled(false);
+        confirmButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(getContext(),
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
-        progressDialog.setMessage(getContext().getString(R.string.authenticating));
+        progressDialog.setMessage(getContext().getString(R.string.confirming));
         progressDialog.show();
 
-        String email = editText.getText().toString();
-        String password = passwordText.getText().toString();
+        String auth_code = editText.getText().toString();
 
         // TODO: Implement authentication logic
 
@@ -101,7 +82,7 @@ public class Login extends Fragment {
                 new Runnable() {
                     public void run() {
                         // On complete call either onConfirmSuccess or onConfirmFailed
-                        onLoginSuccess();
+                        onConfirmSuccess();
                         // onConfirmFailed();
                         progressDialog.dismiss();
                     }
@@ -111,37 +92,29 @@ public class Login extends Fragment {
 
 
 /*    public void onBackPressed() {
-        //TODO: return to main activity
+        //TODO: return to login/sign up
     }*/
 
-    public void onLoginSuccess() {
-        loginButton.setEnabled(true);
+    public void onConfirmSuccess() {
+        confirmButton.setEnabled(true);
     }
 
-    public void onLoginFailed() {
+    public void onConfirmFailed() {
         Toast.makeText(getContext(), R.string.login_failed, Toast.LENGTH_LONG).show();
 
-        loginButton.setEnabled(true);
+        confirmButton.setEnabled(true);
     }
 
     public boolean validate() {
         boolean valid = true;
 
-        String email = editText.getText().toString();
-        String password = passwordText.getText().toString();
+        String auth_code = editText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editText.setError(getContext().getString(R.string.mail_validation));
+        if (auth_code.isEmpty() || auth_code.length() != 6) {
+            editText.setError(getContext().getString(R.string.be_6_numbers));
             valid = false;
         } else {
             editText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            passwordText.setError(getContext().getString(R.string.between_4_10));
-            valid = false;
-        } else {
-            passwordText.setError(null);
         }
 
         return valid;
