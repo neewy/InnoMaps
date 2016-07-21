3b1b97d7c7541e1f82e63f62594528557e137b82
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-07-21 07:46:36 +0300

    Changed DB version parameter

--- app/src/main/java/com/innopolis/maps/innomaps/db/Constants.java
+++ app/src/main/java/com/innopolis/maps/innomaps/db/Constants.java
@@ -4,7 +4,7 @@ package com.innopolis.maps.innomaps.db;
  * Created by alnedorezov on 7/7/16.
  */
 public final class Constants {
-    public static final int DATABASE_VERSION = 9;
+    public static final int DATABASE_VERSION = 10;
     public static final String DATABASE_NAME = "innomapsMobile" + DATABASE_VERSION + ".db";
 
     public static final String DAO_ERROR = "DAO_ERROR";
